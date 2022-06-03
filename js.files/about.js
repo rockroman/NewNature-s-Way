b6dc63f84e683497911a9343928b2f97d1053860
rockroman <44123568+rockroman@users.noreply.github.com>
2022-06-03 13:53:32 +0100

    Add files via upload

---
+++ js.files/about.js
@@ -0,0 +1,17 @@
+let signInBtn=document.getElementById("sign-in").textContent;
+let registerBtn=document.getElementById("register").textContent;
+let modal=document.getElementById("form-container");
+var main=document.querySelector(".main");
+
+function showModal(){
+    modal.style.display="block";
+    
+    main.style.filter="blur(7px)";
+   
+   
+    
+}
+function closeModal(){
+    modal.style.display="none";
+    main.style.filter="";
+}
